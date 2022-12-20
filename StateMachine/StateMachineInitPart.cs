34de2361c8b5d18b98ad21781c1970a4fcd06916
44East <antswert@gmail.com>
2022-12-20 17:19:26 +0300

    Cut unusable code, add Method for Exit with display scores

--- StateMachine/StateMachineInitPart.cs
+++ StateMachine/StateMachineInitPart.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace StoneScissorsPaper
 {
-    internal class StateMachineInitPart
+    internal partial class StateMachine
     {
+        private List<Shape> shapes = new List<Shape>();
+        private Dictionary<States, List<Response>> responses = new Dictionary<States, List<Response>>();
+        private Dictionary<States, string> menuMessages = new Dictionary<States, string>();
+
+        private void InitializeMenuMessages()
+        {
+            menuMessages.Clear();
+            #region Output responses
+            menuMessages.Add(States.Start, textMessages.OpeningMessage);
+            menuMessages.Add(States.MainMenu, textMessages.MainMenuMessage);
+            menuMessages.Add(States.GameMenu, textMessages.GameMenuMessage);
+            #endregion
+        }
+        private void ShapesInitialize()
+        {
+            shapes.Add(stone);
+            shapes.Add(scissors);
+            shapes.Add(paper);
+            stone.Reflector += MessageReflector.ShowAppearance;
+            scissors.Reflector += MessageReflector.ShowAppearance;
+            paper.Reflector += MessageReflector.ShowAppearance;
+        }
+        private void Initialize()
+        {
+
+            #region Input responses
+            List<Response> startResponses = new List<Response>()
+            {
+                new Response{RuKey = "язык",EnKey = "language" , Action = (()=> SetSystemLanguage()) },
+                new Response{RuKey = "меню", EnKey = "menu", Action = (()=>GetMainMenu())},
+                new Response{RuKey = "выход", EnKey = "exit", Action = (()=> Exit()) },
+            };
+            responses.Add(States.Start, startResponses);
+            List<Response> mainMenuStates = new List<Response>()
+            {
+                new Response{RuKey = "играть", EnKey = "play", Action = (()=> PlayGame()) },
+                new Response{RuKey = "назад", EnKey = "back", Action = (()=> BackToLastMenu())},
+                new Response{RuKey = "очки", EnKey = "scores", Action = (()=> OpenScoreMenu())},
+                new Response{RuKey = "выход", EnKey = "exit", Action = (()=> ExitFromGame())}
+            };
+            responses.Add(States.MainMenu, mainMenuStates);
+            List<Response> gameMenu = new List<Response>()
+            {
+                new Response{RuKey = "назад", EnKey = "back", Action = (()=> BackToLastMenu())},
+                new Response{RuKey = "очки", EnKey = "scores", Action = (()=> OpenScoreMenu())},
+                new Response{RuKey = "камень", EnKey = "stone", Action = (()=> SelectFigure(stone))},
+                new Response{RuKey = "ножницы", EnKey = "scissors", Action = (()=> SelectFigure(scissors))},
+                new Response{RuKey = "бумага", EnKey = "paper", Action = (()=> SelectFigure(paper))}
+            };
+            responses.Add(States.GameMenu, gameMenu);
+            #endregion
+        }
     }
 }
--- StateMachine/StateMachineWorkPart.cs
+++ StateMachine/StateMachineWorkPart.cs
@@ -7,67 +7,8 @@ using System.Threading.Tasks;
 
 namespace StoneScissorsPaper
 {
-    internal partial class StateMachine
-    {
-        private List<Shape> shapes = new List<Shape>();
-        private Dictionary<States, List<Response>> responses = new Dictionary<States, List<Response>>();
-        private Dictionary<States, string> menuMessages = new Dictionary<States, string>();
-
-        private void InitializeMenuMessages()
-        {
-            menuMessages.Clear();
-            #region Output responses
-            menuMessages.Add(States.Start, textMessages.OpeningMessage);
-            menuMessages.Add(States.MainMenu, textMessages.MainMenuMessage);
-            menuMessages.Add(States.GameMenu, textMessages.GameMenuMessage);
-            #endregion
-        }
-        private void ShapesInitialize()
-        {
-            shapes.Add(stone);
-            shapes.Add(scissors);
-            shapes.Add(paper);
-            stone.Reflector += MessageReflector.ShowAppearance;
-            scissors.Reflector += MessageReflector.ShowAppearance;
-            paper.Reflector += MessageReflector.ShowAppearance;
-        }
-        private void Initialize()
-        {
-
-            #region Input responses
-            List<Response> startResponses = new List<Response>()
-            {
-                new Response{RuKey = "язык",EnKey = "language" , Action = (()=> SetSystemLanguage()) },
-                new Response{RuKey = "меню", EnKey = "menu", Action = (()=>GetMainMenu())},
-                new Response{RuKey = "выход", EnKey = "exit", Action = (()=> Exit()) },
-            };
-            responses.Add(States.Start, startResponses);
-            List<Response> mainMenuStates = new List<Response>()
-            {
-                new Response{RuKey = "играть", EnKey = "play", Action = (()=> PlayGame()) },
-                new Response{RuKey = "назад", EnKey = "back", Action = (()=> BackToLastMenu())},
-                new Response{RuKey = "очки", EnKey = "scores", Action = (()=> OpenScoreMenu())},
-                new Response{RuKey = "выход", EnKey = "exit", Action = (()=> ExitFromGame())}
-            };
-            responses.Add(States.MainMenu, mainMenuStates);
-            List<Response> gameMenu = new List<Response>()
-            {
-                new Response{RuKey = "назад", EnKey = "back", Action = (()=> BackToLastMenu())},
-                new Response{RuKey = "очки", EnKey = "scores", Action = (()=> OpenScoreMenu())},
-                new Response{RuKey = "камень", EnKey = "stone", Action = (()=> SelectFigure(stone))},
-                new Response{RuKey = "ножницы", EnKey = "scissors", Action = (()=> SelectFigure(scissors))},
-                new Response{RuKey = "бумага", EnKey = "paper", Action = (()=> SelectFigure(paper))}
-            };
-            responses.Add(States.GameMenu, gameMenu);
-            #endregion
-
-
-
-
-
-
-        }
-    }
+    
+    
     internal partial class StateMachine
     {
         private States state;
@@ -166,19 +107,10 @@ namespace StoneScissorsPaper
             }
             MoveNext();
         }
-
-        private void GetChoiceInMainMenu()
-        {
-
-        }
         private void GetMainMenu()
         {
             state = States.MainMenu;
             GetName();
-        }
-        private void ExitFromGame()
-        {
-
         }
         private void GetName()
         {
@@ -204,6 +136,12 @@ namespace StoneScissorsPaper
             else if (state == States.MainMenu)
                 state = States.Start;
             MoveNext();
+        }
+        private void ExitFromGame()
+        {
+            userPerson.GetTheGameScore();
+            pcPerson.GetTheGameScore();
+            Exit();
         }
         private void Exit() { return; }
 
